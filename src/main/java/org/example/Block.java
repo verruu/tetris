2beb480571ba2783f30f74aa970e87f9a3c7d9b2
verruu <gacek333@gmail.com>
2023-09-08 14:25:04 +0200

    Tetris - second attempt with GDX v1.0.1.

--- src/main/java/org/example/Block.java
+++ src/main/java/org/example/Block.java
@@ -1,17 +1,23 @@
 package org.example;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class Block extends Image {
     private Texture texture;
+    private TextureRegion textureRegion;
+    private TextureRegionDrawable textureRegionDrawable;
 
     public Block() {
         // Load your block texture
-        texture = new Texture("block.png");
+        texture = new Texture("resources/pattern.png");
+        textureRegion = new TextureRegion(texture);
+        textureRegionDrawable = new TextureRegionDrawable(textureRegion);
 
         // Set the texture as the drawable for the Image
-        setDrawable(texture);
+        setDrawable(textureRegionDrawable);
     }
 
     public void clearBlock() {
@@ -26,9 +32,9 @@ public class Block extends Image {
     }
 
     // If you override dispose, remember to call super.dispose() if you have other resources to dispose of.
-    @Override
-    public void dispose() {
-        clearBlock();
-        super.dispose();
-    }
+//    @Override
+//    public void dispose() {
+//        clearBlock();
+//        super.dispose();
+//    }
 }
